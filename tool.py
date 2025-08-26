a85fd07e17c376f226530d0086c1c734b1426518
LORD-SANTINO <mikkyrose297@gmail.com>
2025-08-26 21:00:48 +0100

    Update tool.py

--- tool.py
+++ tool.py
@@ -53,7 +53,7 @@ async def message_handler(update: Update, context: ContextTypes.DEFAULT_TYPE) ->
     context.user_data.clear()
 
 def main():
-    app = ApplicationBuilder().token("YOUR_TELEGRAM_BOT_TOKEN").build()
+    app = ApplicationBuilder().token("8377001447:AAEUZMLRP346KzuxLOpAVJdGzUTJ1g-KM9k").build()
 
     app.add_handler(CommandHandler("start", start))
     app.add_handler(CallbackQueryHandler(button_handler))
